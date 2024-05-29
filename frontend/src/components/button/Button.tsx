8618feb64e2a59fb92cf8d45c2ee7944ce70ae41
agusb19 <agustinbrage19@gmail.com>
2024-05-29 18:19:34 -0300

    feat: add hover-state on hero button for a 1.5s at first render
    (change button text to get FREE version)

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -1,16 +1,25 @@
+import { useRef, useEffect } from 'react'
 import './button.css'
 
 export function ButtonAction() {
 
+    const buttonRef = useRef<HTMLButtonElement>(null)
+
+    useEffect(() => {
+        setTimeout(() => {
+            buttonRef.current?.classList.remove('hover-state')
+        }, 1500)
+    }, [])
+
     return (
         <div className='button-container'>
-            <button className="animated-button" type='button'>
+            <button ref={buttonRef} className="animated-button hover-state" type='button'>
                 <svg viewBox="0 0 24 24" className="arr-2" xmlns="http://www.w3.org/2000/svg">
                     <path
                     d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
                     ></path>
                 </svg>
-                <span className="text">Get it now</span>
+                <span className="text">Get FREE version</span>
                 <span className="circle"></span>
                 <svg viewBox="0 0 24 24" className="arr-1" xmlns="http://www.w3.org/2000/svg">
                     <path
