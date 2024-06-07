6e1d4fbfdabc1decb5bf4d491f7ea243a7da687f
agusb19 <agustinbrage19@gmail.com>
2024-06-07 11:45:47 -0300

    refactor: remove all backend files, prepare to migrate to hono

--- backend/server/global/services/notFoundHandler.js
+++
@@ -1,6 +0,0 @@
-import { CustomError } from "../helpers/customError.js";
-
-export const notFoundHandler = (req, _res, next) => {
-    const err = new CustomError(`Cannot find ${req.originalUrl} on the server!`, 404);
-    next(err);
-}
