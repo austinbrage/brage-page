a0c9973fa99561a2882c1a04b05adbe30b8d6caf
agusb19 <agustinbrage19@gmail.com>
2024-06-10 19:18:26 -0300

    feat: fetch mercadopago api instead of sdk on services

--- backend/src/services/mercadopago.ts
+++ backend/src/services/mercadopago.ts
@@ -1,53 +1,105 @@
-import { MercadoPagoConfig, Preference, Payment } from 'mercadopago'
-import { MP_ACCESS_TOKEN, HOST_URL } from '../utils/config'
-
 type Items = {
     id: string
     title: string
     quantity: number
+    description: string
+    currency_id: string
     unit_price: number
 }
 
-export class MercadoPagoService {
-    private mpClient: MercadoPagoConfig
+type PaymentResponse = { 
+    status: string, 
+    additional_info: { 
+        items: Array<{
+            id: string, 
+            title: string 
+        }> 
+    } 
+}
 
-    constructor(private payerName?: string, private payerEmail?: string) {
-        this.mpClient = new MercadoPagoConfig({ accessToken: MP_ACCESS_TOKEN })
-    }
+type PreferenceResponse = { 
+    init_point: string 
+}
 
-    private async getPreferenceInitPoint(item: Items) {
-        const preference = await new Preference(this.mpClient).create({
-            body: { 
-                items: [item], 
-                payer: { 
-                    name: this.payerName, 
-                    email: this.payerEmail 
-                },
-                back_urls: {
-                    success: `${HOST_URL}/products/javascript/confirm`,
-                    failure: `${HOST_URL}/products/javascript/failure`,
-                    pending: `${HOST_URL}/products/javascript/pending`
-                },
-                auto_return: 'approved'
-            }
+export class MpPaymentService {
+
+    constructor(private accessToken: string) {}
+
+    async getPayment(paymentId: string) {
+        const response = await fetch(`https://api.mercadopago.com/v1/payments/${paymentId}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${this.accessToken}`,
+            },
         })
 
-        return preference.sandbox_init_point
-    }
+        if (!response.ok) {
+            const errorText = await response.text()
+            throw new Error(`At creating payment details [${response.statusText}, ${errorText}]`)
+        }
 
-    async getPayment(paymentId: number) {
-        const payment = await new Payment(this.mpClient).get({ id: paymentId })
+        const data = await response.json() as PaymentResponse
 
         return {
-            isApproved: payment.status === 'approved',
-            productId: payment.additional_info?.items?.[0]?.id
+            isApproved: data.status === 'approved',
+            productId: data.additional_info?.items?.[0]?.id,
+            productTitle: data.additional_info?.items?.[0]?.title
         }
+    }
+}
+
+export class MpPreferenceService {
+
+    constructor(
+        private frontHostUrl: string,
+        private accessToken: string, 
+        private payerName: string, 
+        private payerEmail: string
+    ) {}
+
+    private getPreferenceData(item: Items) {
+        return {
+            items: [item], 
+            payer: { 
+                name: this.payerName, 
+                email: this.payerEmail 
+            },
+            back_urls: {
+                success: `${this.frontHostUrl}/payment-success`,
+                failure: `${this.frontHostUrl}/payment-failure`,
+                pending: `${this.frontHostUrl}/payment-pending`
+            },
+            auto_return: 'approved'
+        }
+    } 
+
+    private async getPreferenceInitPoint(item: Items) {
+        const preference = this.getPreferenceData(item)
+
+        const response = await fetch('https://api.mercadopago.com/checkout/preferences', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${this.accessToken}`,
+            },
+            body: JSON.stringify(preference),
+        })
+
+        if (!response.ok) {
+            const errorText = await response.text()
+            throw new Error(`At creating preference [${response.statusText}, ${errorText}]`)
+        }
+
+        const data = await response.json() as PreferenceResponse
+        return data?.init_point
     }
 
     async buyBasicJS() {
         return await this.getPreferenceInitPoint({
-            id: 'basic_javascript_product',
+            id: 'brage-javascript-tool-basic.zip',
             title: 'Basic Javascript BRAGE',
+            description: 'BRAGE JavaScript tool for beginners',
+            currency_id: 'USD',
             quantity: 1,
             unit_price: 94.55
         })
@@ -55,8 +107,10 @@ export class MercadoPagoService {
 
     async buySqlSaferJS() {
         return await this.getPreferenceInitPoint({
-            id: 'sqlsafer_javascript_product',
+            id: 'brage-javascript-tool-sqlcmd.zip',
             title: 'SQL Safer Javascript BRAGE',
+            description: 'BRAGE JavaScript tool with sql command',
+            currency_id: 'USD',
             quantity: 1,
             unit_price: 124.55
         })
@@ -64,8 +118,10 @@ export class MercadoPagoService {
 
     async buyDbGenaratorJS() {
         return await this.getPreferenceInitPoint({
-            id: 'dbgenerator_javascript_product',
+            id: 'brage-javascript-tool-dbcmd.zip',
             title: 'DB Generator Javascript BRAGE',
+            description: 'BRAGE JavaScript tool with db command',
+            currency_id: 'USD',
             quantity: 1,
             unit_price: 154.55
         })
@@ -73,8 +129,10 @@ export class MercadoPagoService {
 
     async buyFullGenaratorJS() {
         return await this.getPreferenceInitPoint({
-            id: 'fullgenerator_javascript_product',
+            id: 'brage-javascript-tool-complete.zip',
             title: 'Fullstack Generator Javascript BRAGE',
+            description: 'BRAGE JavaScript complete tool',
+            currency_id: 'USD',
             quantity: 1,
             unit_price: 184.55
         })
