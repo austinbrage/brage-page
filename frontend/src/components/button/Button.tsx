5a33ecac6830e879e7c4d79dc2c25516310b1829
agusb19 <agustinbrage19@gmail.com>
2024-06-10 21:56:16 -0300

    feat: use action button to purchase basic js version

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -1,14 +1,7 @@
 import { useRef, useEffect, useContext } from 'react'
 import { PaymentContext } from '../../contexts/payments'
-import { type Products } from '../../data/products'
+import { JAVASCRIPT_PRODUCTS } from '../../data/products'
 import './button.css'
-
-const FREE_PRODUCT: Products = {
-    name: 'Basic Free',
-    price: 0,
-    suitableFor: 'beginers',
-    features: []
-}
 
 export function ButtonAction() {
 
@@ -18,12 +11,12 @@ export function ButtonAction() {
     
     const handleOpenPayment = () => {
         updateOpenPayment(true)
-        updateCurrentProduct(FREE_PRODUCT)
+        updateCurrentProduct(JAVASCRIPT_PRODUCTS[0])
     }
 
     useEffect(() => {
         setTimeout(() => {
-            buttonRef.current?.classList.remove('hover-state')
+            buttonRef.current?.classList.add('hover-state')
         }, 1500)
     }, [])
 
@@ -33,18 +26,18 @@ export function ButtonAction() {
                 ref={buttonRef} 
                 onClick={() => handleOpenPayment()}
                 type='button'
-                className="animated-button hover-state" 
+                className="animated-button" 
             >
                 <svg viewBox="0 0 24 24" className="arr-2" xmlns="http://www.w3.org/2000/svg">
                     <path
-                    d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+                        d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
                     ></path>
                 </svg>
-                <span className="text">Get FREE version</span>
+                <span className="text">Get BASIC version</span>
                 <span className="circle"></span>
                 <svg viewBox="0 0 24 24" className="arr-1" xmlns="http://www.w3.org/2000/svg">
                     <path
-                    d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+                        d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
                     ></path>
                 </svg>
             </button>
